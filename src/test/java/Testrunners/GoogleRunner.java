babea57002b49e4fd95add20649e5ebf269adc23
DrAlluru <aluruprakash123@gmail.com>
2024-08-17 20:52:10 +0530

    Update GoogleRunner.java

--- src/test/java/Testrunners/GoogleRunner.java
+++ src/test/java/Testrunners/GoogleRunner.java
@@ -8,5 +8,5 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\Admin\\OneDrive\\Desktop\\staragile\\Manual Testing\\Wellsforgocompany\\CucumberfwEve\\src\\test\\resources\\Featurefiles\\A.feature",glue="stepdef")
 public class GoogleRunner {
-
+Yes Boss I am team lead
 }
